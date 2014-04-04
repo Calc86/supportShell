32077aeed52f350cb61aca08c93f219d0fc3bda0
calc <calc@list.ru>
2014-04-04 14:30:55 +0400

    server side

---
+++ server/index.php
@@ -0,0 +1,20 @@
+<html>
+    <body>
+        <a href="./files/user.jar">user.jar</a><br>
+        <a href="./files/user.exe">user.exe</a><br>
+        <a href="./files/jre-7u51-windows-x64.exe">jre-7u51-windows-x64.exe</a><br>
+        <a href="./files/jre-7u51-windows-i586.exe">jre-7u51-windows-i586.exe</a><br>
+        <BR><BR>
+        <a href="./cmd.php">cmd.php</a><br>
+        <a href="./cmd_post.php">cmd_post.php</a><br>
+    </body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calc
+ * Date: 03.04.14
+ * Time: 14:18
+ */
+
